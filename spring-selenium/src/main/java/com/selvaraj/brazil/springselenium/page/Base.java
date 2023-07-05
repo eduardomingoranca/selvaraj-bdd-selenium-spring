184183b83b5e7770b15a8cb151275a1e696d4e5c
eduardomingoranca <eduardomingoranca@gmail.com>
2023-07-05 19:18:12 -0300

    creating base class

---
+++ spring-selenium/src/main/java/com/selvaraj/brazil/springselenium/page/Base.java
@@ -0,0 +1,24 @@
+package com.selvaraj.brazil.springselenium.page;
+
+import jakarta.annotation.PostConstruct;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.openqa.selenium.support.PageFactory.initElements;
+
+public abstract class Base {
+
+    @Autowired
+    protected WebDriver driver;
+
+    @Autowired
+    protected WebDriverWait wait;
+    @PostConstruct
+    private void init() {
+        initElements(this.driver, this);
+    }
+
+    public abstract boolean isAt();
+
+}
