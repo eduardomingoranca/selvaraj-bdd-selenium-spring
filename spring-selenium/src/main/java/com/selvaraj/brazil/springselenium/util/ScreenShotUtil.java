76c6bf2714c71ef1c69559ad3c4496d0f35632ee
eduardomingoranca <eduardomingoranca@gmail.com>
2023-07-06 20:37:46 -0300

    screen shot util - spring component - parte one

---
+++ spring-selenium/src/main/java/com/selvaraj/brazil/springselenium/util/ScreenShotUtil.java
@@ -0,0 +1,30 @@
+package com.selvaraj.brazil.springselenium.util;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.TakesScreenshot;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+
+import static org.openqa.selenium.OutputType.FILE;
+import static org.springframework.util.FileCopyUtils.copy;
+
+@Component
+public class ScreenShotUtil {
+
+    @Autowired
+    private TakesScreenshot driver;
+
+    @Value("${screenshot.path}/img.png")
+    private Path path;
+
+    public void takeScreenShot() throws IOException {
+        File sourceFile = this.driver.getScreenshotAs(FILE);
+        copy(sourceFile, this.path.toFile());
+    }
+
+}
--- spring-selenium/src/test/java/com/selvaraj/brazil/springselenium/googletest/GoogleTest.java
+++ spring-selenium/src/test/java/com/selvaraj/brazil/springselenium/googletest/GoogleTest.java
@@ -2,8 +2,11 @@ package com.selvaraj.brazil.springselenium.googletest;
 
 import com.selvaraj.brazil.springselenium.SpringBaseTestNGTest;
 import com.selvaraj.brazil.springselenium.page.google.GooglePage;
+import com.selvaraj.brazil.springselenium.util.ScreenShotUtil;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.io.IOException;
 
 import static org.testng.Assert.assertTrue;
 
@@ -12,14 +15,18 @@ public class GoogleTest extends SpringBaseTestNGTest {
     @Autowired
     private GooglePage googlePage;
 
+    @Autowired
+    private ScreenShotUtil screenShotUtil;
+
     @Test
-    public void googleTest() throws InterruptedException {
+    public void googleTest() throws IOException {
         this.googlePage.goTo();
         assertTrue(this.googlePage.isAt());
 
         this.googlePage.getSearchComponent().search("spring boot");
         assertTrue(this.googlePage.getSearchResult().isAt());
         assertTrue(this.googlePage.getSearchResult().getCount() > 2);
+        this.screenShotUtil.takeScreenShot();
     }
 
 }
