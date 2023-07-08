fa04d26a52698eab69b9eeda04e239a3daf4c39b
eduardomingoranca <eduardomingoranca@gmail.com>
2023-07-08 17:28:22 -0300

    creating custom scope for webdriver

---
+++ spring-selenium/src/main/java/com/selvaraj/brazil/springselenium/config/BrowserScope.java
@@ -0,0 +1,29 @@
+package com.selvaraj.brazil.springselenium.config;
+
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.remote.SessionId;
+import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.context.support.SimpleThreadScope;
+
+import static java.util.Objects.isNull;
+
+public class BrowserScope extends SimpleThreadScope {
+
+    @Override
+    public Object get(String name, ObjectFactory<?> objectFactory) {
+        Object o = super.get(name, objectFactory);
+
+        SessionId sessionId = ((RemoteWebDriver) o).getSessionId();
+        if (isNull(sessionId)) {
+            super.remove(name);
+            o = super.get(name, objectFactory);
+        }
+
+        return o;
+    }
+
+    @Override
+    public void registerDestructionCallback(String name, Runnable callback) {
+
+    }
+}
---
+++ spring-selenium/src/main/java/com/selvaraj/brazil/springselenium/config/BrowserScopePostProcessor.java
@@ -0,0 +1,14 @@
+package com.selvaraj.brazil.springselenium.config;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+
+public class BrowserScopePostProcessor implements BeanFactoryPostProcessor {
+    @Override
+    public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
+        BrowserScope scope = new BrowserScope();
+        beanFactory.registerScope("browserscope", scope);
+    }
+
+}
