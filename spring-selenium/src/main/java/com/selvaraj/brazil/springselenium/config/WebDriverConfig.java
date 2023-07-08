f75817a591f3ea73f92e3d40b46b519915fc1575
eduardomingoranca <eduardomingoranca@gmail.com>
2023-07-08 14:08:13 -0300

    injecting unique bean - conditional on property

--- spring-selenium/src/main/java/com/selvaraj/brazil/springselenium/config/WebDriverConfig.java
+++ spring-selenium/src/main/java/com/selvaraj/brazil/springselenium/config/WebDriverConfig.java
@@ -5,10 +5,10 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
-import org.springframework.context.annotation.Primary;
 
 import static io.github.bonigarcia.wdm.WebDriverManager.chromedriver;
 import static io.github.bonigarcia.wdm.WebDriverManager.firefoxdriver;
@@ -21,13 +21,14 @@ public class WebDriverConfig {
     private int timeout;
 
     @Bean
+    @ConditionalOnProperty(name = "browser", havingValue = "chrome")
     public WebDriver chromeDriver() {
         chromedriver().version("113.0.5672.63").setup();
         return new ChromeDriver();
     }
 
     @Bean
-    @Primary
+    @ConditionalOnProperty(name = "browser", havingValue = "firefox")
     public WebDriver firefoxDriver() {
         firefoxdriver().setup();
         return new FirefoxDriver();
