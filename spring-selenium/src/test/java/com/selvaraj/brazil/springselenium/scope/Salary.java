31ee25ad8532e47cdabc6e2574e3606a89e20ad0
eduardomingoranca <eduardomingoranca@gmail.com>
2023-07-08 16:36:20 -0300

    bean scope - singleton vs prototype

--- spring-selenium/src/test/java/com/selvaraj/brazil/springselenium/scope/Salary.java
+++ spring-selenium/src/test/java/com/selvaraj/brazil/springselenium/scope/Salary.java
@@ -1,8 +1,10 @@
 package com.selvaraj.brazil.springselenium.scope;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope("prototype")
 public class Salary {
     private int amount;
 
