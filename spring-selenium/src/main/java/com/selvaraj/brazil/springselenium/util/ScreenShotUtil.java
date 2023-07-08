961917196b82f8aa9dd541b894c3aec8191ed323
eduardomingoranca <eduardomingoranca@gmail.com>
2023-07-08 18:02:45 -0300

    spring application context - demo

--- spring-selenium/src/main/java/com/selvaraj/brazil/springselenium/util/ScreenShotUtil.java
+++ spring-selenium/src/main/java/com/selvaraj/brazil/springselenium/util/ScreenShotUtil.java
@@ -4,6 +4,7 @@ import com.github.javafaker.Faker;
 import org.openqa.selenium.TakesScreenshot;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
@@ -19,7 +20,7 @@ import static org.springframework.util.FileCopyUtils.copy;
 public class ScreenShotUtil {
 
     @Autowired
-    private TakesScreenshot driver;
+    private ApplicationContext ctx;
 
     @Value("${screenshot.path}")
     private Path path;
@@ -28,7 +29,7 @@ public class ScreenShotUtil {
     private Faker faker;
 
     public void takeScreenShot() throws IOException {
-        File sourceFile = this.driver.getScreenshotAs(FILE);
+        File sourceFile = this.ctx.getBean(TakesScreenshot.class).getScreenshotAs(FILE);
         String imageName = faker.name().firstName();
         copy(sourceFile, this.path.resolve(imageName + ".png").toFile());;
     }
