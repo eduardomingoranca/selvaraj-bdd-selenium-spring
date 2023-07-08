abe3a64c0f8fc852c3b0df28467c4723492cf43b
eduardomingoranca <eduardomingoranca@gmail.com>
2023-07-08 15:17:02 -0300

    executing tests in local / remote grid using spring profile - part one

---
+++ spring-selenium/src/main/java/com/selvaraj/brazil/springselenium/config/RemoteWebDriverConfig.java
@@ -0,0 +1,45 @@
+package com.selvaraj.brazil.springselenium.config;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+
+import java.net.URL;
+
+import static org.openqa.selenium.remote.DesiredCapabilities.chrome;
+import static org.openqa.selenium.remote.DesiredCapabilities.firefox;
+
+@Lazy
+@Configuration
+public class RemoteWebDriverConfig {
+
+    @Value("${selenium.grid.url}")
+    private URL url;
+
+    @Value("${default.timeout:30}")
+    private int timeout;
+
+    @Bean
+    @ConditionalOnProperty(name = "browser", havingValue = "firefox")
+    public WebDriver remoteFirefoxDriver() {
+        return new RemoteWebDriver(this.url, firefox());
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public WebDriver remoteChromeDriver() {
+        return new RemoteWebDriver(this.url, chrome());
+    }
+
+    @Bean
+    public WebDriverWait webDriverWait(WebDriver driver) {
+        return new WebDriverWait(driver, this.timeout);
+    }
+
+}
