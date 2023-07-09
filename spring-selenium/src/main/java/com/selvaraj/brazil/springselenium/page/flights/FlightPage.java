f52a2a1be3fc07b34c8cb55b8c93721becee2328
eduardomingoranca <eduardomingoranca@gmail.com>
2023-07-09 12:20:37 -0300

    google flight application - page object

---
+++ spring-selenium/src/main/java/com/selvaraj/brazil/springselenium/page/flights/FlightPage.java
@@ -0,0 +1,35 @@
+package com.selvaraj.brazil.springselenium.page.flights;
+
+import com.selvaraj.brazil.springselenium.kelvin.annotation.Page;
+import com.selvaraj.brazil.springselenium.page.Base;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+
+@Page
+public class FlightPage extends Base {
+
+    @FindBy(css = "nav.rlGvde a")
+    private List<WebElement> elements;
+
+    public void goTo(final String url) {
+        this.driver.get(url);
+    }
+
+    public List<String> getLabels() {
+       return this.elements
+                .stream()
+                .map(WebElement::getText)
+                .map(String::trim)
+                .collect(toList());
+    }
+
+    @Override
+    public boolean isAt() {
+        return this.wait.until((d) -> !this.elements.isEmpty());
+    }
+
+}
