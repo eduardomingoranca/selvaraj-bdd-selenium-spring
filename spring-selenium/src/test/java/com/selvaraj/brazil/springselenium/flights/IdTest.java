1b961422069b44aa4e2091596dcd64694002c0a4
eduardomingoranca <eduardomingoranca@gmail.com>
2023-07-09 13:06:03 -0300

    spring boot - annotation testpropertysource - part two

--- spring-selenium/src/test/java/com/selvaraj/brazil/springselenium/flights/IdTest.java
+++ spring-selenium/src/test/java/com/selvaraj/brazil/springselenium/flights/IdTest.java
@@ -2,7 +2,7 @@ package com.selvaraj.brazil.springselenium.flights;
 
 import org.springframework.test.context.TestPropertySource;
 
-@TestPropertySource(properties = "app.locale=id")
+@TestPropertySource(properties = {"app.locale=id", "browser=firefox"})
 public class IdTest extends FlightTest {
 
 }
