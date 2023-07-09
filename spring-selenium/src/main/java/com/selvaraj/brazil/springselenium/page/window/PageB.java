0cfcd33b9deefc114d57ac4779c0da3438e53fe3
eduardomingoranca <eduardomingoranca@gmail.com>
2023-07-09 13:53:46 -0300

    page object creation for additional windows

---
+++ spring-selenium/src/main/java/com/selvaraj/brazil/springselenium/page/window/PageB.java
@@ -0,0 +1,24 @@
+package com.selvaraj.brazil.springselenium.page.window;
+
+import com.selvaraj.brazil.springselenium.kelvin.annotation.Page;
+import com.selvaraj.brazil.springselenium.page.Base;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+@Page
+public class PageB extends Base {
+
+    @FindBy(id = "area")
+    private WebElement textArea;
+
+    public void addToArea(final String msg) {
+        this.textArea.sendKeys(msg);
+    }
+
+
+    @Override
+    public boolean isAt() {
+        return this.wait.until((d) -> this.textArea.isDisplayed());
+    }
+
+}
