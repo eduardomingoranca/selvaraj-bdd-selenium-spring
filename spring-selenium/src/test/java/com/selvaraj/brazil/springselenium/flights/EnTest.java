5529ab948198645f2423fce38b8cc54dac8ebbec
eduardomingoranca <eduardomingoranca@gmail.com>
2023-07-09 12:59:21 -0300

    spring boot - annotation testpropertysource - part one

---
+++ spring-selenium/src/test/java/com/selvaraj/brazil/springselenium/flights/EnTest.java
@@ -0,0 +1,9 @@
+package com.selvaraj.brazil.springselenium.flights;
+
+import org.springframework.test.context.TestPropertySource;
+
+@TestPropertySource(properties = "app.locale=en")
+public class EnTest extends FlightTest{
+
+
+}
---
+++ spring-selenium/src/test/java/com/selvaraj/brazil/springselenium/flights/IdTest.java
@@ -0,0 +1,8 @@
+package com.selvaraj.brazil.springselenium.flights;
+
+import org.springframework.test.context.TestPropertySource;
+
+@TestPropertySource(properties = "app.locale=id")
+public class IdTest extends FlightTest {
+
+}
