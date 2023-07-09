4523c0d1349c474965ee6a09807d6655796cf597
eduardomingoranca <eduardomingoranca@gmail.com>
2023-07-09 16:47:33 -0300

    visa test - demo - part one

--- spring-selenium/src/test/java/com/selvaraj/brazil/springselenium/CustomerVisaTest.java
+++ spring-selenium/src/test/java/com/selvaraj/brazil/springselenium/CustomerVisaTest.java
@@ -1,17 +1,42 @@
 package com.selvaraj.brazil.springselenium;
 
+import com.selvaraj.brazil.springselenium.entity.Customer;
+import com.selvaraj.brazil.springselenium.page.visa.VisaRegistrationPage;
 import com.selvaraj.brazil.springselenium.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.testng.annotations.Test;
 
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.List;
+
+import static java.time.ZoneId.systemDefault;
+
 public class CustomerVisaTest extends SpringBaseTestNGTest{
+    @Autowired
+    private VisaRegistrationPage registrationPage;
+
     @Autowired
     private CustomerRepository repository;
 
     @Test
     public void visaTest() {
-        this.repository.findById(85)
-                .ifPresent(u -> System.out.println(u.getFirstName()));
+        List<Customer> customers = this.repository.findAll()
+                .stream()
+                .limit(3)
+                .toList();
+
+        for (Customer c: customers) {
+            this.registrationPage.goTo();
+            this.registrationPage.setNames(c.getFirstName(), c.getLastName());
+            this.registrationPage.setCountryFromAndTo(c.getFromCountry(), c.getToCountry());
+            this.registrationPage.setBirthDate(c.getDob().toInstant().atZone(systemDefault()).toLocalDate());
+            this.registrationPage.setContactDetails(c.getEmail(), c.getPhone());
+            this.registrationPage.setComments(c.getComments());
+            this.registrationPage.submit();
+
+            System.out.println(this.registrationPage.getConfirmationNumber());
+        }
     }
 
 }
