63f296279792f2bcd75671ba29bc196d33fc1596
eduardomingoranca <eduardomingoranca@gmail.com>
2023-07-10 20:13:05 -0300

    running tests in parallel

--- spring-selenium/src/test/java/com/selvaraj/brazil/springselenium/CucumberRunner.java
+++ spring-selenium/src/test/java/com/selvaraj/brazil/springselenium/CucumberRunner.java
@@ -2,6 +2,7 @@ package com.selvaraj.brazil.springselenium;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.DataProvider;
 
 @CucumberOptions(
         features = "classpath:features",
@@ -12,5 +13,11 @@ import io.cucumber.testng.CucumberOptions;
         }
 )
 public class CucumberRunner extends AbstractTestNGCucumberTests {
+
+        @Override
+        @DataProvider(parallel = true)
+        public Object[][] scenarios() {
+                return super.scenarios();
+        }
 
 }
