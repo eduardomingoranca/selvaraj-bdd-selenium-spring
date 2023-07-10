6bc85c7a60871aa3c475bf3020be0bfb59fd906a
eduardomingoranca <eduardomingoranca@gmail.com>
2023-07-09 22:46:42 -0300

    steps definition creation for google test - part one

---
+++ spring-selenium/src/test/java/com/selvaraj/brazil/springselenium/bdd/GoogleSteps.java
@@ -0,0 +1,26 @@
+package com.selvaraj.brazil.springselenium.bdd;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class GoogleSteps {
+
+    @Given("I am on the google site")
+    public void iAmOnTheGoogleSite() {
+    }
+
+    @When("I enter {string} as a keyword")
+    public void iEnterAsAKeyword(String arg0) {
+    }
+
+    @And("I click on the search button")
+    public void iClickOnTheSearchButton() {
+    }
+
+    @Then("I should see at least {string} results")
+    public void iShouldSeeAtLeastResults(String arg0) {
+    }
+
+}
